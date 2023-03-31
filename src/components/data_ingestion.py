11b89c94462dc1f48dbab85093d2b33e1f03964c
iamrosan <roshanshrestha21.63@gmail.com>
2023-03-31 09:11:54 +0545

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path : str = os.path.join('artifacts','train.csv')
@@ -48,4 +48,9 @@ if __name__ == '__main__':
 
     
     data_transformation_obj = DataTransformation()
-    data_transformation_obj.initiate_data_transformation(train_data, test_data)
+    # data_transformation_obj.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation_obj.initiate_data_transformation(train_data, test_data)
+
+
+    model_trainer = ModelTrainer()
+    print("r2_score: ",model_trainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr))
